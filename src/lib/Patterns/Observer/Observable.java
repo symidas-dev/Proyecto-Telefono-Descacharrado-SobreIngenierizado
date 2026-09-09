874c4e32126d3dede92c78a099cedef79ad8065f
midas@air <javier.liano@alumnos.uneatlantico.es>
2026-09-09 16:52:53 +0200

    feat(observer): creado patrón observer interface

---
+++ src/lib/Patterns/Observer/Observable.java
@@ -0,0 +1,9 @@
+package lib.Patterns.Observer;
+
+public interface Observable {
+    public void addObserver(Observer observer);
+
+    public void removeObserver(Observer observer);
+
+    public void notifyAllObservers();
+}
---
+++ src/lib/Patterns/Observer/Observer.java
@@ -0,0 +1,5 @@
+package lib.Patterns.Observer;
+
+public interface Observer {
+    public void update();
+}
